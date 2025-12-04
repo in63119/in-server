e0164aa47ce72e50f516d9553972e34a2500f101
이인수 <in63119@gmail.com>
2025-12-04 16:19:01 +0900

    feat: load env-scoped config from SSM JSON

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,43 @@ package config
 import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
-	Port string `envconfig:"PORT" default:":4000"`
 	Env  string `envconfig:"ENV" default:"development"`
+	Port string `envconfig:"PORT" default:":4000"`
+
+	Auth struct {
+		Hash string `envconfig:"AUTH_HASH"`
+		JWT  struct {
+			AccessSecret string `envconfig:"AUTH_JWT_ACCESS_SECRET"`
+		}
+	}
+
+	AWS struct {
+		AccessKey       string `envconfig:"AWS_SSM_ACCESS_KEY"`
+		SecretAccessKey string `envconfig:"AWS_SSM_SECRET_KEY"`
+		Region          string `envconfig:"AWS_REGION" default:"ap-northeast-2"`
+		Param           string `envconfig:"AWS_SSM_SERVER"`
+		S3        struct {
+			Bucket    string `envconfig:"AWS_S3_BUCKET"`
+			AccessKey string `envconfig:"AWS_S3_ACCESS_KEY_ID"`
+			SecretKey string `envconfig:"AWS_S3_SECRET_ACCESS_KEY"`
+		}
+	}
+
+	Firebase struct {
+		ProjectID   string `envconfig:"FIREBASE_PROJECT_ID"`
+		ClientEmail string `envconfig:"FIREBASE_CLIENT_EMAIL"`
+		PrivateKey  string `envconfig:"FIREBASE_PRIVATE_KEY"`
+		DatabaseURL string `envconfig:"FIREBASE_DATABASE_URL"`
+	}
+
+	Blockchain struct {
+		PrivateKey struct {
+			Owner    string `envconfig:"BLOCKCHAIN_PRIVATE_KEY_OWNER"`
+			Relayer  string `envconfig:"BLOCKCHAIN_PRIVATE_KEY_RELAYER"`
+			Relayer2 string `envconfig:"BLOCKCHAIN_PRIVATE_KEY_RELAYER2"`
+			Relayer3 string `envconfig:"BLOCKCHAIN_PRIVATE_KEY_RELAYER3"`
+		}
+	}
 }
 
 func Load() (Config, error) {
